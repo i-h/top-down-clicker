2a0eb47efced1e604d05a1500461a3b24a1d4f9e
Teemu Kokkonen <teemu.kokkonen@koudata.fi>
2016-03-07 12:12:57 +0200

    Coins

---
+++ Assets/Script/MoneyCoin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoneyCoin : MonoBehaviour {
+    public float moneyYield = 1.0f;
+    void OnTriggerEnter(Collider c)
+    {
+        Debug.Log(c.tag);
+        if (c.gameObject.CompareTag("Player"))
+        {
+            PlayerData.money += moneyYield;
+            Destroy(gameObject);
+        }
+    }
+}
